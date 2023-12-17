f06983dde19b606c4426d1aa8cd321190a6d3e77
DemonLord- <1441529822@qq.com>
2023-12-17 23:48:53 +0800

    Set up

---
+++ src/main/java/com/example/trainticketsystem/TrainTicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainTicketSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trainticketsystem/TrainTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
